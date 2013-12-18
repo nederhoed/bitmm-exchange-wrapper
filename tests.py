673e3a40d4e1f000b767da338806fbbf5ab4a502
eris <johnnydebris@gmail.com>
2013-12-18 14:58:06 +0100

    Revert "Updated tests after changing api."
    This reverts commit 4992cceea48d988adca5509ac94a36d09d2becc8.

--- tests.py
+++ tests.py
@@ -170,8 +170,8 @@ class XWrapTestCase(XWrapTestBase):
         ]
         backend = self.account.backend(1)
         self.assertEquals(backend.id, 1)
-        self.assertEquals(backend.apikey, 'foo')
-        self.assertEquals(backend.apisecret, 'bar')
+        self.assertEquals(backend.username, 'foo')
+        self.assertEquals(backend.password, 'bar')
 
     def test_backend_balance(self):
         self.responses = [
